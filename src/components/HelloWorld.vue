de341726c1702d1c43aeab460274245ecaf98d45
suriyarichard <chinnaraj2061971@gmail.com>
2022-05-07 14:55:48 +0530

    bot

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -52,6 +52,7 @@
 import chatWindow from "./chatWindow.vue";
 export default {
   name: "helloWorld",
+
   components: {
     chatWindow,
   },
@@ -70,6 +71,16 @@ export default {
       console.log("launchPopup", this.cw);
     },
   },
+   mounted (){
+    (function(d, m){
+        var kommunicateSettings = 
+            {"appId":"1181a0b83a265bec89a9fcdcfc3bacfc1","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+   },
 };
 </script>
 
